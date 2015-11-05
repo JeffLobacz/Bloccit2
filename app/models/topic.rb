d89c263152d53b01706190109da04ab6d189f98d
Jeff <jeff.lobacz@gmail.com>
2015-11-04 16:31:01 -0800

    Labels Checkpoint successfully completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,5 +1,10 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
+  has_many :labelings, as: :labelable
+  has_many :labels, through: :labelings
+
+  # The following default_scope was added by Jeff for testing purposes
+  default_scope { order('created_at DESC') }
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true

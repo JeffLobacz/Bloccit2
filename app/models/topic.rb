d103dec30d27e1fc55020468641cd43c7fd1dec1
Jeff <jeff.lobacz@gmail.com>
2015-11-05 15:14:00 -0800

    Voting Checkpoint successfully completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,7 +4,7 @@ class Topic < ActiveRecord::Base
   has_many :labels, through: :labelings
 
   # The following default_scope was added by Jeff for testing purposes
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
 
   validates :name, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 15 }, presence: true

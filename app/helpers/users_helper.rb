9f292eea17477d6fcf17d0c305ff0d543a8e7629
Jeff <jeff.lobacz@gmail.com>
2015-11-14 01:37:36 -0800

    Commit made only to ask for help

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+
+  def user_has_posts(user)
+    user.posts.count >= 1
+  end
+
+  def user_has_comments(user)
+    user.comments.count >= 1
+  end
+
+  def user_has_favorites(user)
+    user.favorites.count >= 1
+  end
+
 end

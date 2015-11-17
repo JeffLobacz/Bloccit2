d999b05f993f27885c1badc25654eca4e46f98c1
Jeff <jeff.lobacz@gmail.com>
2015-11-17 13:29:42 -0800

    Incomplete commit as of Tuesday Nov17

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,11 @@
 module PostsHelper
+
   def user_is_authorized_for_post?(post)
     current_user && (current_user == post.user || current_user.admin?)
   end
+
+  # def total_votes(post)
+  #   total_votes = post.votes.up_votes.count + post.votes.down_votes.count
+  # end
+
 end

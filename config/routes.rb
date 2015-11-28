6c94f38f75085f33563561a6a83011a1aeb669ec
Jeff <jeff.lobacz@gmail.com>
2015-11-27 19:18:50 -0800

    API-Retrieving Checkpoint successfully completed

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,14 @@ Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
 
   get 'about' => 'welcome#about'
+
   root to: 'welcome#index'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:index, :show]
+      resources :topics, only: [:index, :show]
+    end
+  end
 
 end

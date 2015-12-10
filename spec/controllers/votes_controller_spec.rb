f1055ca465d89a701858d78e32e345e46f9ff7a5
Jeff <jeff.lobacz@gmail.com>
2015-12-09 21:33:53 -0800

    Ajax assignment successfully completed

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -55,17 +55,17 @@ RSpec.describe VotesController, type: :controller do
         expect(user_post.points).to eq(points + 1)
       end
 
-      # it "returns http success" do
-      #   # request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
-      #   post :up_vote, format: :js, post_id: user_post.id, id: my_vote.id
-      #   expect(response).to have_http_status(:success)
-      # end
+      it "returns http success" do
+        request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
+        post :up_vote, format: :js, post_id: user_post.id
+        expect(response).to have_http_status(:success)
+      end
 
-      # it ":back redirects to posts topic show" do
-      #   request.env["HTTP_REFERER"] = topic_path(my_topic)
-      #   post :up_vote, format: :js, post_id: user_post.id
-      #   expect(response).to redirect_to(my_topic)
-      # end
+      it "returns http success" do
+        request.env["HTTP_REFERER"] = topic_path(my_topic)
+        post :up_vote, format: :js, post_id: user_post.id
+        expect(response).to have_http_status(:success)
+      end
 
     end
 
@@ -90,17 +90,17 @@ RSpec.describe VotesController, type: :controller do
         expect(user_post.points).to eq(points - 1)
       end
 
-      # it ":back redirects to posts show page" do
-      #   request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
-      #   post :down_vote, format: :js, post_id: user_post.id
-      #   expect(response).to redirect_to([my_topic, user_post])
-      # end
-      #
-      # it ":back redirects to posts topic show" do
-      #   request.env["HTTP_REFERER"] = topic_path(my_topic)
-      #   post :down_vote, format: :js, post_id: user_post.id
-      #   expect(response).to redirect_to(my_topic)
-      # end
+      it "returns http success" do
+        request.env["HTTP_REFERER"] = topic_post_path(my_topic, user_post)
+        post :down_vote, format: :js, post_id: user_post.id
+        expect(response).to have_http_status(:success)
+      end
+
+      it "returns http success" do
+        request.env["HTTP_REFERER"] = topic_path(my_topic)
+        post :down_vote, format: :js, post_id: user_post.id
+        expect(response).to have_http_status(:success)
+      end
 
     end
 
